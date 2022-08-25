cdba3e6cff163a7df8eb3f4dbc5decdb8b7ebc47
taylorshocklee <99515099+taylorshocklee@users.noreply.github.com>
2022-08-25 16:26:27 -0500

    Delete ListUsersGroups.ps1

--- ListUsersGroups.ps1
+++
@@ -1,9 +0,0 @@
-#This script will list all groups a user is in.
-
-$User = Read-Host -Prompt 'Enter username: '
-$Groups = (Get-ADUser $User -Property memberof).memberof
-$Final = ForEach ($group in $Groups) 
-{
-        (Get-ADGroup $group).name
-}
-Write-Output $Final | Sort-Object
