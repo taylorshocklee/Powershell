a0b7c318f736f5cf5508ad9270be40b429025d8e
taylorshocklee <99515099+taylorshocklee@users.noreply.github.com>
2022-08-25 16:26:32 -0500

    Delete ListComputersGroups.ps1

--- ListComputersGroups.ps1
+++
@@ -1,7 +0,0 @@
-$Computer = Read-Host -Prompt 'Enter computername: '
-$Groups = (Get-ADComputer $Computer -Property memberof).memberof
-$Final = ForEach ($group in $Groups) 
-{
-        (Get-ADGroup $group).name
-}
-Write-Output $Final | Sort-Object
